9146ff30b21ce5d9e738d66d25b1542999ceb53e
prasanna-12 <78677332+prasanna-12@users.noreply.github.com>
2022-05-12 18:23:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "antd/dist/antd.css";
+import './App.css';
+import AdminUI from "./components/index";
+
+function App() {
+  return (
+    <div className="admin__app">
+      <AdminUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "antd/dist/antd.css";
+import EditTable from "./EditTable";
+
+const AdminUI = () => {
+  return (
+    <div className="admin__content">
+      <EditTable />
+    </div>
+  );
+};
+
+export default AdminUI;
